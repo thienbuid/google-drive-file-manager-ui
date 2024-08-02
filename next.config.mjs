719af0a75e5e88b5454fc961512c7b226157165b
BuiThien <thienbuiduy.work@gmail.com>
2024-08-02 23:03:52 +0700

    [setup] config app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["drive-thirdparty.googleusercontent.com"],
+  },
+};
 
 export default nextConfig;
