0a8aa820833dfeff4bd3ebe685b467424d80939a
BuiThien <thienbuiduy.work@gmail.com>
2024-08-05 01:11:32 +0700

    [fix] fix ui file card

--- app/components/Files/FileCard.tsx
+++ app/components/Files/FileCard.tsx
@@ -9,12 +9,12 @@ type Props = {
 
 const FileCard = ({ file }: Props) => {
   return (
-    <div className="flex flex-col bg-[#F0F4F8] hover:bg-[#d9dcdf] rounded-lg flex-1 p-4 gap-4 h-[300px]">
+    <div className="flex flex-col bg-[#F0F4F8] hover:bg-[#d9dcdf] rounded-lg flex-1 p-4 gap-4 h-[300px] hover:cursor-pointer focus:bg-pink-300">
       <div className="flex justify-between items-center flex-row gap-4">
         <div>
           <img src={file.iconLink} alt="" />
         </div>
-        <p className="flex flex-1 truncate">{file.name}</p>
+        <p className="flex flex-1 truncate select-none">{file.name}</p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
@@ -29,6 +29,7 @@ const FileCard = ({ file }: Props) => {
           <img
             src={`https://lh3.googleusercontent.com/d/${file.id}=w1000-h1000?authuser=0`}
             alt=""
+            loading="lazy"
           />
         </div>
       )}
