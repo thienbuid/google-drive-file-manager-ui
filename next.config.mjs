ba190e12df6b8f40018a1a0d84988e05d36fc8b3
BuiThien <thienbuiduy.work@gmail.com>
2024-08-12 16:42:48 +0700

    [fix] fix config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "drive-thirdparty.googleusercontent.com",
-      "lh3.googleusercontent.com",
-    ],
-  },
+    images: {
+        domains: [
+            'drive-thirdparty.googleusercontent.com',
+            'lh3.googleusercontent.com',
+            'https://drive.google.com',
+        ],
+    },
 };
 
 export default nextConfig;
