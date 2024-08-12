ebe3e3e0bb9730e96304bc9486b95c4dd50ec068
BuiThien <thienbuiduy.work@gmail.com>
2024-08-12 16:43:09 +0700

    [fix] fix layout app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import GoogleDriveManager from "./components/GoogleDriveManager";
+import GoogleDriveManager from "./components/FileManager";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between sm:p-0 2xl:p-24 bg-black text-gray-950 break-words">
+    <main className="flex h-screen flex-col items-center justify-between sm:p-0 2xl:p-24 bg-black text-gray-950 break-words">
       <GoogleDriveManager />
     </main>
   );
